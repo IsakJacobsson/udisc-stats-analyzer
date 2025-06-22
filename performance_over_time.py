edc72dfa46bb9365e9f0f2e7e22f4877905a9209
Isak Jacobsson <isak2jacobsson@gmail.com>
2025-06-22 20:46:37 +0200

    performance_over_time.py: Refactoring

--- performance_over_time.py
+++ performance_over_time.py
@@ -6,12 +6,12 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 import pandas as pd
 
-def generate_dataframe(csv_dir):
+def generate_dataframes(csv_dir):
     # Load all CSV files from a directory
     csv_files = glob.glob(os.path.join(csv_dir, "*.csv"))
 
-    dfs = []
-    par_dfs = []
+    result_df = pd.DataFrame()
+    result_par_df = pd.DataFrame()
 
     for file in csv_files:
         df = pd.read_csv(file)
@@ -19,99 +19,78 @@ def generate_dataframe(csv_dir):
         # Normalize smart quotes in all string columns (e.g., PlayerName)
         for col in df.select_dtypes(include=['object']).columns:
             df[col] = df[col].str.replace('[“”]', '"', regex=True)
-
-        # Get par row separately
-        par_df = df[df["PlayerName"] == "Par"]
-
-        if not par_df.empty:
-            # Melt par row
-            par_df["Total"] = pd.to_numeric(par_df["Total"], errors="coerce")
-
-            nbr_holes = len([col for col in par_df.columns if col.startswith("Hole")])
-            for hole in range(1, nbr_holes+1):
-                par_df[f"Hole{hole}"] = pd.to_numeric(par_df[f"Hole{hole}"], errors="coerce")
-            
-            par_df["StartDate"] = pd.to_datetime(par_df["StartDate"])
-
-            par_dfs.append(par_df)
-
         
-        df_fixed = df[df["PlayerName"] != "Par"]
+        # Set type for StartDate and EndDate
+        df["StartDate"] = pd.to_datetime(df["StartDate"])
+        df["EndDate"] = pd.to_datetime(df["EndDate"])
 
-        df_fixed["Total"] = pd.to_numeric(df_fixed["Total"], errors="coerce")
-
-        nbr_holes = len([col for col in df.columns if col.startswith("Hole")])
-        for hole in range(1, nbr_holes+1):
-            df_fixed[f"Hole{hole}"] = pd.to_numeric(df_fixed[f"Hole{hole}"], errors="coerce")
-        
-        for index, row in df_fixed.iterrows():
+        # Set Total to zero if the round wasn't completed
+        holes = [col for col in df.columns if col.startswith("Hole")]
+        for index, row in df.iterrows():
             round_finished = True
-            for hole in range(1, nbr_holes+1):
-                if row[f"Hole{hole}"] == 0:
+            for hole in holes:
+                if row[hole] == 0:
                     round_finished = False
                     break
             if  not round_finished:
-                df_fixed.at[index, "Total"] = 0
-
-        df_fixed["StartDate"] = pd.to_datetime(df_fixed["StartDate"])
+                df.at[index, "Total"] = 0
+    
+        # Concat to result_par_df
+        par_df = df[df["PlayerName"] == "Par"].copy()
+        par_df = par_df.drop(columns=["StartDate", "EndDate"])
+        result_par_df = pd.concat([result_par_df, par_df], ignore_index=True)
         
-        dfs.append(df_fixed)
+        # Conat to result_df
+        df = df[df["PlayerName"] != "Par"]
+        result_df = pd.concat([result_df, df], ignore_index=True)
 
-    # Combine all df_long DataFrames
-    df = pd.concat(dfs, ignore_index=True)
-    par_df = pd.concat(par_dfs, ignore_index=True)
+    # No need for multiple rows of the same course and layout
+    result_par_df = result_par_df.drop_duplicates()
 
-    return df, par_df
+    return result_df, result_par_df
 
-def graph_performance(df, par_df, course_name, layout_name, players, stat, output_path, plot_par):
+def filter_df_by_players(df, players):
+    return df[df['PlayerName'].isin(players)]
+
+def filter_df(df, course_name, layout_name, stat):
     # Filter the DataFrame
-    subset = df[
+    df = df[
         (df["CourseName"] == course_name) &
         (df["LayoutName"] == layout_name)
-    ].copy()
+    ]
 
-    subset = subset[subset[stat] != 0]
+    df = df[df[stat] != 0]
 
-    if subset.empty:
-        print(f"No data found for Course: '{course_name}', Layout: '{layout_name}'")
-        return
+    return df
 
+def graph_performance(df, par_df, course_name, layout_name, players, stat, output_path, plot_par):
     sns.set_theme(style="ticks", palette="pastel")
 
-    if players[0] != "all":
-        subset = subset[subset['PlayerName'].isin(players)]
-    else:
-        players = list(subset["PlayerName"].unique())
-
-    for name in players:
-        sub_subset = subset[subset["PlayerName"] == name]
-
-        if subset.empty:
-            print(f"No data found for player '{name}'")
-            continue
+    for player in players:
+        player_df = df[df["PlayerName"] == player]
 
         # Plot stat for player
-        sns.lineplot(data=sub_subset, x="StartDate", y=stat, label=name, marker='o', alpha=0.8)
+        sns.lineplot(data=player_df, x="StartDate", y=stat, label=player, marker='o', alpha=0.8)
     
     if plot_par:
-        par_subset = par_df[
-            (par_df["CourseName"] == course_name) &
-            (par_df["LayoutName"] == layout_name)
-        ].copy()
-
-        par_subset = par_subset[par_subset['StartDate'].isin(subset['StartDate'])]
-        
-        sns.lineplot(data=par_subset, x="StartDate", y=stat, label="Par", linewidth=2.5, alpha=0.8, color="green", linestyle="--")
+        plt.axhline(y=par_df.loc[0, stat], label='Par', linewidth=2.5, alpha=0.8, color="green", linestyle="--")
     
     plt.title(f"Performance over time for {stat} on {course_name}, {layout_name}")
     plt.grid(True)
-
     if output_path:
         plt.savefig(output_path, dpi=100)
     plt.show()
 
 def main(args, players):
-    df, par_df = generate_dataframe(args.csv_dir)
+    df, par_df = generate_dataframes(args.csv_dir)
+    
+    df = filter_df(df, args.course, args.layout, args.stat)
+    if players[0] == "all":
+        players = list(df["PlayerName"].unique())
+    df = filter_df_by_players(df, players)
+
+    par_df = filter_df(par_df, args.course, args.layout, args.stat)
+
     graph_performance(df, par_df, args.course, args.layout, players, args.stat, args.output, args.plot_par)
 
 if __name__ == "__main__":
